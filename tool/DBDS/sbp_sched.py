c604ec0b6219cd0aa42bb8eaa2d623b57c5b2b5a
Ruiv <ruixuelicn@163.com>
2024-05-08 14:20:52 +0800

    v3.0：传统方法实现

--- tool/DBDS/sbp_sched.py
+++ tool/DBDS/sbp_sched.py
@@ -30,8 +30,10 @@ for i in s:
 '''
 
 class SBPSched(BPSched):
+    # sched:前缀
+    # pattern:前缀的骨架
     def __init__(self, n = 1, kps = [0], p = 1, sched = None, pattern = None):
-        Sched._init_helper(self, n, kps, p, sched)
+        Sched._init_helper(self, n, kps, p, sched) # 初始化_sched和_pattern
         for i in self._sched:
             if not i:
                 raise ValueError("Invalid period(s) in the given sched!")
@@ -39,38 +41,46 @@ class SBPSched(BPSched):
         self._iter = self._first_iter
         lastPattern = -1
         pre = 1
+        # yfadd
         mustnot=set()
         self._start = len(self._sched)
+        # yfaddend
+        # 处理有前缀的情况，进行初始化
         if pattern or sched:
             self._init_helper(self._n, self._kps, self._p, sched, pattern)
             if self._sched:
                 lastPattern = self._pattern[-1]
                 pre = len(self._sched)
-                self._last = self._sched[-1][-1]
-                self._lastCount = len(self._sched[-1])
-                self._initsched =self._sched.copy()
-                for j in self._pattern:
+                # yfadd
+                self._last = self._sched[-1][-1]        #前缀最后一个关键点
+                self._lastCount = len(self._sched[-1])   #前缀最后一个周期的关键点个数
+                self._initsched =self._sched.copy()     #拷贝前缀
+                for j in self._pattern:                 # 检查前缀中降序情况，如果有，加到mustnot中
                     if j > lastPattern:
                         mustnot.add(j)
+                # yfaddend
+
+        # 调用init_pattern生成前缀之后的骨架
         if self._p != len(self._pattern):
-            patterncopy=self._pattern.copy()
-            sub_pattern = self._init_pattern(
+            patterncopy=self._pattern.copy()  # yfadd
+            sub_pattern = self._init_pattern(  # 生成前缀之后的骨架
                     self._n,
                     self._kp_pool,
                     self._p - len(self._pattern),
                     set(i for i in range(self._n) if self._kp_pool[i] > 0),
                     lastPattern,
                     mustnot,
-                    patterncopy)
+                    patterncopy) # yfadd最后三个参数
             if not sub_pattern:
                 raise ValueError("Can't finish initialising pattern!")
             # print(sub_pattern)
+            # 生成完整的骨架
             self._pattern += sub_pattern
 
         for i in range(self._n):
             if i not in self._pattern and self._kps[i] > 0:
                 raise ValueError("Not all theads can be scheduled!")
-
+        # 填充关键点，生成最初符合字典序的调度
         self._sched += self._init_sched(
                 self._n, self._kp_pool,
                 self._p - len(self._sched),
@@ -114,6 +124,27 @@ class SBPSched(BPSched):
         if done == None:
             done =[]
 
+        # # rxadd
+        # if p == len(mustDo)-1:
+        #     prefix_pattern = self._pattern.copy()
+        #     last_prefix_pattern = prefix_pattern.pop()
+        #     # 填充kps[last_prefix_pattern[0]]到last_prefix_pattern中
+        #     special_pattern = [last_prefix_pattern[0]] * kps[last_prefix_pattern[0]]
+        #     # special_p = p+1
+        #     # prefix_pattern = self._pattern.copy()
+        #     # # 取出prefix_pattern的最后一个元素
+        #     # last_prefix_pattern = prefix_pattern.pop()
+        #     # # 取出last_prefix_pattern的元素
+        #     # for i in mustDo:
+        #     #     if i == last_prefix_pattern[0]:
+        #     #         #从mustDo中删除last_prefix_pattern的元素
+        #     #         mustDo.remove(i)
+        #     # # special_pattern = [last_prefix_pattern[0]]
+        #     # special_pattern = []
+        #     # special_pattern.append(last_prefix_pattern[0])
+
+
+        # 如果剩余周期数小于要分配的周期数，就无法生成——会出现前缀最后一个周期不能被扩充的问题
         if p < 1 or p < len(mustDo):
             return None
 
@@ -178,14 +209,17 @@ class SBPSched(BPSched):
         return None
 
 
-
+    # 已知上一个pattern，生成下一个pattern，用最小的pattern变化
     def _next_pattern(self,n, kps, p, pattern, mustDo = None, last = -1,mustnot=None,done=None):
         if mustDo == None:
             mustDo = set(i for i in range(n) if kps[i] > 0)
+        # yfadd
         if mustnot == None:
             mustnot = set()
         if done == None:
             done =[]
+        # yfaddend
+
         if p < 1:
             return None
         if p == 1:
@@ -203,21 +237,27 @@ class SBPSched(BPSched):
                     continue
                 if kps[i] <= 0:
                     continue
+                # yfadd
                 if i in mustnot:
                     continue
+                # yfaddend
                 return [i]
             return None
 
         rkps = kps.copy()
         rkps[pattern[0]] -= 1
-        done.append(pattern[0])
+        # yfadd
+        done.append(pattern[0]) # done：已经生成的pattern元素
         mustnotpre=mustnot.copy()
         if pattern[0] < last:
             for j in done:
                 if(j>pattern[0]):
                     mustnot.add(j)
+        # yfaddend
+
+
         sub_pattern = self._next_pattern(
-                n, rkps, p - 1, pattern[1:], mustDo - {pattern[0]}, pattern[0],mustnot,done)
+                n, rkps, p - 1, pattern[1:], mustDo - {pattern[0]}, pattern[0],mustnot,done)# yf改了参数
         if sub_pattern:
             return [pattern[0]] + sub_pattern
         mustnot=mustnotpre.copy()
@@ -290,11 +330,12 @@ class SBPSched(BPSched):
 
     def _first_iter(self):
         self._iter = self._next_iter
+        # 复制了init_sched的结果
         rlt  = [x.copy() for x in self._sched]
         return rlt
 
-    def _next_iter(self):
-        if self._start > 0:
+    def _next_iter(self): # yfupdate
+        if self._start > 0:  # 有前缀的情况
             kp_pool= self._kp_pool.copy()
             kp_pool[self._last] += self._lastCount
             lastPattern=self._pattern[self._start-1]
@@ -302,12 +343,10 @@ class SBPSched(BPSched):
             for j in self._pattern[:self._start]:
                 if j > lastPattern:
                     mustnot.add(j)
-            # p = self._p - self._start + 1
-            # sched = self._sched[self._start - 1:]
-            # pre_sched=self._sched[:self._start-1]
-            # pattern = self._pattern[self._start - 1:]
+            # 在pattern不变的情况下，生成下一个sched
             sched = self._next_sched(self._n, kp_pool, self._p-self._start +1, self._sched[self._start - 1:], self._pattern[self._start - 1:])
             if sched == None:
+                # 在pattern变化的情况下，生成下一个前缀之后的pattern
                 pattern = self._next_pattern(
                     self._n,
                     self._kp_pool,
@@ -322,17 +361,16 @@ class SBPSched(BPSched):
                 if pattern == None:
                     raise StopIteration()
                 else:
+                    # self._pattern是前缀，pattern是前缀之后的pattern
                     self._pattern = self._pattern[:self._start]+pattern
+                    # 在新的骨架上分配关键点，传的参数是前缀之后的pattern
                     sched = self._init_sched(
                             self._n, self._kp_pool, self._p-self._start, self._pattern[self._start:])
                     sched_temp=self._initsched+sched
             else:
+                # self._sched：传进来的前缀 sched：生成前缀后面的调度
                 sched_temp = self._sched[:self._start -1]+ sched
-        else:
-            # kp_pool = self._kp_pool.copy()
-            # p = self._p
-            # sched = self._sched.copy()
-            # pattern = self._pattern.copy()
+        else:  # 没有前缀的情况
             sched = self._next_sched(self._n, self._kp_pool, self._p, self._sched, self._pattern)
             while sched == None:
                 pattern = self._next_pattern(
@@ -345,22 +383,13 @@ class SBPSched(BPSched):
                     sched = self._init_sched(
                             self._n, self._kps, self._p, self._pattern)
             sched_temp =sched
-        # while sched_chosen == None:
-        #     pattern = self._next_pattern(
-        #             self._n,
-        #             self._ckps,
-        #             self._p - self._start,
-        #             self._pattern[self._start:],
-        #             set(self._chosen),
-        #             -1 if self._start == 0 else self._pattern[self._start - 1])
-        # sched = self._next_sched(
-        #         self._n, self._kps, self._p,
-        #         self._sched, self._pattern)
+        # 更新新生成的调度sched的前缀
         self._prefix = []
         for i in range(len(sched_temp)):
             self._prefix.append(sched_temp[i])
             if sched_temp[i] != self._sched[i]:
                 break
+        # 完整的调度
         self._sched =sched_temp
         rlt  = [x.copy() for x in self._sched]
         return rlt
